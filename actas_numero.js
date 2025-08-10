6afd8418cbe625d665c8befd26b71df2ae435227
PacoChinchay <pacochinchay350@gmail.com>
2025-08-09 21:40:02 -0500

    feat: find by acta´s number

--- actas_numero.js
+++ actas_numero.js
@@ -37,7 +37,6 @@ const actas_bscarPrNmroMesa = async (form) => {
                       </div>
                       <div class="col-xs-9 col-md-8">
                         <div class="row">
-
                           <div class="col-xs-12">
                             <p class="subtitle1">ACTA ELECTORAL</p>
                             <div id="page-wrap">
@@ -57,7 +56,6 @@ const actas_bscarPrNmroMesa = async (form) => {
                               </table>
                             </div>
                           </div>
-
                           <div class="col-xs-12">
                             <p class="subtitle1">INFORMACIÓN UBIGEO</p>
                             <div id="page-wrap">
@@ -81,7 +79,6 @@ const actas_bscarPrNmroMesa = async (form) => {
                               </table>
                             </div>
                           </div>
-
                           <div class="col-xs-12">
                             <p class="subtitle1">INFORMACIÓN MESA</p>
                             <div id="page-wrap">
@@ -101,47 +98,59 @@ const actas_bscarPrNmroMesa = async (form) => {
                               </table>
                             </div>
                           </div>
-
-                          <div class="col-xs-12 pbot30_acta">
-                            <p class="subtitle1">LISTA DE RESOLUCIONES</p>
-                            <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> No hay resoluciones para el acta seleccionada
-                            <div id="page-wrap">
-                              <div class="col-md-12 resolu"></div>
-                            </div>
-                          </div>
-
-                          <div class="col-xs-12">
-                            <p class="subtitle1">INFORMACIÓN DEL ACTA ELECTORAL</p>
-                            <div id="page-wrap" class="cont-tabla1">
-                              <table class="table06">
-                                <tbody>
-                                  <tr class="titulo_tabla">
-                                    <td colspan="2">Organización política</td>
-                                    <td>Total de Votos</td>
-                                  </tr>
-                                  <tr>
-                                    <td>PERUANOS POR EL KAMBIO</td>
-                                    <td><img width="40px" height="40px" src="images/simbolo_pkk.jpg"></td>
-                                    <td>${acta.P1}</td>
-                                  </tr>
-                                  <tr>
-                                    <td>FUERZA POPULAR</td>
-                                    <td><img width="40px" height="40px" src="images/simbolo_keyko.jpg"></td>
-                                    <td>${acta.P2}</td>
-                                  </tr>
-                                  <tr><td colspan="2">VOTOS EN BLANCO</td><td>${acta.VotosBlancos}</td></tr>
-                                  <tr><td colspan="2">VOTOS NULOS</td><td>${acta.VotosNulos}</td></tr>
-                                  <tr><td colspan="2">VOTOS IMPUGNADOS</td><td>${acta.VotosImpugnados}</td></tr>
-                                  <tr><td colspan="2">TOTAL DE VOTOS EMITIDOS</td><td>${acta.TotalVotantes}</td></tr>
-                                </tbody>
-                              </table>
-                            </div>
-                          </div>
-
                         </div>
                       </div>
                     </div>
-
+                    <div>
+                      <div class="col-xs-12 pbot30_acta">
+                        <p class="subtitle1">LISTA DE RESOLUCIONES</p>
+                        <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> No hay resoluciones para el acta seleccionada
+                        <div id="page-wrap">
+                          <div class="col-md-12 resolu"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div class="col-xs-12">
+                        <p class="subtitle1">INFORMACIÓN DEL ACTA ELECTORAL</p>
+                        <div id="page-wrap" class="cont-tabla1">
+                          <table class="table06">
+                            <tbody>
+                              <tr class="titulo_tabla">
+                                <td colspan="2">Organización política</td>
+                                <td>Total de Votos</td>
+                              </tr>
+                              <tr>
+                                <td>PERUANOS POR EL KAMBIO</td>
+                                <td><img width="40px" height="40px" src="images/simbolo_pkk.jpg"></td>
+                                <td>${acta.P1}</td>
+                              </tr>
+                              <tr>
+                                <td>FUERZA POPULAR</td>
+                                <td><img width="40px" height="40px" src="images/simbolo_keyko.jpg"></td>
+                                <td>${acta.P2}</td>
+                              </tr>
+                              <tr>
+                                <td colspan="2">VOTOS EN BLANCO</td>
+                                <td>${acta.VotosBlancos}</td>
+                              </tr>
+                              <tr>
+                                <td colspan="2">VOTOS NULOS</td>
+                                <td>${acta.VotosNulos}</td>
+                              </tr>
+                              <tr>
+                                <td colspan="2">VOTOS IMPUGNADOS</td>
+                                <td>${acta.VotosImpugnados}</td>
+                              </tr>
+                              <tr>
+                                <td colspan="2">TOTAL DE VOTOS EMITIDOS</td>
+                                <td>${acta.TotalVotantes}</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
